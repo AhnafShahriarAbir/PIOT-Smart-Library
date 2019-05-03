48ce59840a92d4ec55f3e46cb1440f4358d321dd
s3599490@student.rmit.edu.au <s3599490@student.rmit.edu.au>
2019-05-03 23:49:30 +1000

    Update validation.py
    -update validation algorithm
    -set email as primary key
    -loggin by email and password

--- validation.py
+++ validation.py
@@ -1,29 +1,38 @@
 #!/usr/bin/env python3
 import sqlite3
-from bcrypt import hashpw, gensalt,checkpw
+from passlib.hash import sha256_crypt
+from validate_email import validate_email
 import sys
 import re
 databaseName='/home/pi/A2/profile.db'
 conn=sqlite3.connect(databaseName)
 
 class validate():
+    #The check_username() requirement user enter their user name,length should be less than 7,valid input is number,character ,"_""-"
     def check_username(self):
-        username=input("Enter your username : ")
         while True:
+            username=input("Enter your username : ")
             if len(username) < 7:
                 if re.match("^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]*$", username) != None:
                     print("Valid user name")
                     break
             else :
+                False
                 print("Invalid user name")
             
         self.username=username
         return (self.username)
-    
+    #The check_password() will ask user enter password for twice,the password will be :
+    # length between 6 to 12 
+    # at least one character 
+    # at least one number 
+    # at least one symbol between "[@#$]" 
+    # space is not allowed
     def check_password(self):
         while True:
             password=input("Enter your password : ")
             confirmed_password=input("Enter your password again: ")   
+
             if (len(password)<6 or len(password)>12):
                 print("password length is between 6 to 12,please check and enter again")
 
@@ -43,21 +52,19 @@ class validate():
                 print("Incorrect password mathcing")
 
             else:
-                True
                 print("valid password")
                 break
+        hashedPassword = sha256_crypt.hash(password)
 
-        hashPassword=hashpw(password.encode('utf8'), gensalt(13))
-        #print(hashPassword)
-        self.hashPassword=hashPassword
-        self.password=password
-        return (self.hashPassword,self.password)
+        self.hashedPassword=hashedPassword
 
+        return (self.hashedPassword)
+
+    #The check_name() will ask user to enter first name and last name, then check if it is alphabetically
     def check_name(self):
-        fname=input("Enter your first name : ")
-        lname=input("Enter your last name : ")
-        
         while True:
+            fname=input("Enter your first name : ")
+            lname=input("Enter your last name : ")
             name=fname+lname
             if name.isalpha():
                 full_name=fname+" "+lname
@@ -68,38 +75,49 @@ class validate():
        
         return (self.name)
 
+    #The check_email() will ask user enter email address and check the style
     def check_email(self):
-        email=input("Enter your email :")
         while True:
-
-            if re.match("^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]*$", email) is None:
+            email=input("Enter your email :")
+            '''curs=conn.cursor()
+            curs.execute('SELECT * FROM profile_user WHERE Email =(?)',(email,))
+            result = curs.fetchone()
+            if(str(email) == result[3]):
+                False
+                print("Email is already used,please use another one.")'''
+            if (re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", email) ):
                 print("Valid email address")
-                break
-            elif len(email)< 7:
-                print("Invalid email address")              
+                #conn.close()
+                break          
             else:
-                print("Invalid email address")
+                False
+                print("Invalid email,try again")
+
         self.email=email
 
         return (self.email)
 
+    #The check_login_tatus() will ask user to enter their signed email address and password,
     def check_login_status(self):
-        userName=input("Enter your username : ")
-        passWord=input("Enter password : ")
-        #hashPassWord=hashpw(passWord.encode('utf8'), gensalt(13))
-        #print(hashPassWord)
-        #if (checkpw(passWord, hashPassWord)) is not None:
-            #print("Correct Password")
-        curs=conn.cursor()
-        curs.execute('SELECT * FROM profile_user_login WHERE username =(?) AND password=(?)',(userName,passWord,))
-        if curs.fetchone() is None:            
-            print ("Login failed,try again")
-        else:
-            print ("Welcome!"+userName)
-            
+        while True:
+            user_email=input("Enter your email address : ")
+            passWord=input("Enter password : ")
+            curs=conn.cursor()
+            curs.execute('SELECT * FROM profile_user WHERE Email =(?)',(user_email,))
+            results = curs.fetchone()
+            print(results)
+            if results is None:
+                print("User is not found!")
+            else:
+                    if sha256_crypt.verify(passWord,results[1]):            
+                        print ("Welcome!"+results[0])
+                        break
+                    else:
+                        print ("Incorrect password,please check!")
+                
             #break
 
-        self.userName=userName
+        self.user_email=user_email
         self.passWord=passWord
-        return(self.userName,self.passWord)
+        return(self.user_email,self.passWord)
         
