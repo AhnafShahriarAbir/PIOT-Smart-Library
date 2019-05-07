0de351cb73c6c5567f77cad15ae7ddc9a6b0fa44
s3599490@student.rmit.edu.au <s3599490@student.rmit.edu.au>
2019-05-07 19:39:51 +1000

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,51 +1,47 @@
 #!/usr/bin/env python3
 import sqlite3
+from bcrypt import hashpw, gensalt,checkpw
 import sys
+
 from validation import validate
 from passlib.hash import sha256_crypt
-from client import Client
-
-databaseName = 'profile.db'
-conn = sqlite3.connect(databaseName)
-
+databaseName='/home/pi/A2/profile.db'
+conn=sqlite3.connect(databaseName)
 
 class menu():
     def display_menu(self):
         while True:
-            print(30 * "-", "MENU", 30 * "-")
-            print("Welcome\n")
-            print("Please select you want to start:")
-            print("1. Sign Up ")
-            print("2. Log in ")
-            print("\n0. Quit")
-            print(67 * "-")
-            choice = input("Enter your choice: ")
-            check = validate()
-            if choice == ("1"):
-                username = check.check_username()
-                hashPassword = check.check_password()
-                name = check.check_name()
-                email = check.check_email()
-                curs = conn.cursor()
-                curs.execute("INSERT INTO profile_user VALUES ((?),(?),(?),(?))", (username, hashPassword, name, email))
+            print (30 * "-" , "MENU" , 30 * "-")
+            print ("Welcome\n")
+            print ("Please select you want to start:")
+            print ("1. Sign Up ")
+            print ("2. Log in ")
+            print ("\n0. Quit")
+            print (67 * "-")
+            choice=input("Enter your choice: ")    
+            check=validate()
+            if choice ==("1"):
+                username=check.check_username()
+                hashPassword=check.check_password()
+                name=check.check_name()
+                email=check.check_email()
+                curs=conn.cursor()
+                curs.execute("INSERT INTO profile_user VALUES ((?),(?),(?),(?))", (username,hashPassword,name,email))
                 conn.commit()
                 print("\nSigned up,please log in\n")
-
-            elif choice == ("2"):
-                user_Email = check.check_login_status()
                 
-                Client.get_details(user_Email)
-
-            elif choice == ("0"):
+            elif choice==("2"):
+                user_Email,passWord=check.check_login_status()
+                
+            elif choice==("0"):
                 sys.exit()
 
-            else:
+            else :
                 print("Invalid selection,please enter number 1 or 2 or 0")
                 return
 
-
 def main():
-    a = menu()
+    a=menu()
     a.display_menu()
 
 main()
