7caa14ceede54939accb884e5035feefd6de6b31
s3599490@student.rmit.edu.au <s3599490@student.rmit.edu.au>
2019-05-14 17:13:17 +1000

    minor change in library

--- library_menu.py
+++ library_menu.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python3
 import sqlite3
 import sys
-import socket
-databaseName = 'profile.db'
-conn = sqlite3.connect(databaseName)
-HOST = "192.168.0.9"
-# HOST = "127.0.0.1" # The server's hostname or IP address.
-PORT = 65001         # The port used by the server.
-ADDRESS = (HOST, PORT)
+
 
 class library_menu():
     def display_menu(user_email):
