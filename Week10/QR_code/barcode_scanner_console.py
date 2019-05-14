0d8422d0b18d3f4175080c71173ecf663ddc997d
AhnafShahriarAbir <s3577189@student.rmit.edu.au>
2019-05-15 03:45:01 +1000

    qr code and speech not done yet

--- Week10/QR_code/barcode_scanner_console.py
+++ Week10/QR_code/barcode_scanner_console.py
@@ -13,6 +13,7 @@ import datetime
 import imutils
 import time
 import cv2
+from databaseUtils import DatabaseUtils
 
 # initialize the video stream and allow the camera sensor to warm up
 print("[INFO] starting video stream...")
@@ -23,27 +24,30 @@ found = set()
 
 # loop over the frames from the video stream
 while True:
-	# grab the frame from the threaded video stream and resize it to
-	# have a maximum width of 400 pixels
-	frame = vs.read()
-	frame = imutils.resize(frame, width = 400)
+    # grab the frame from the threaded video stream and resize it to
+    # have a maximum width of 400 pixels
+    frame = vs.read()
+    frame = imutils.resize(frame, width = 400)
 
-	# find the barcodes in the frame and decode each of the barcodes
-	barcodes = pyzbar.decode(frame)
+    # find the barcodes in the frame and decode each of the barcodes
+    barcodes = pyzbar.decode(frame)
 
-	# loop over the detected barcodes
-	for barcode in barcodes:
-		# the barcode data is a bytes object so we convert it to a string
-		barcodeData = barcode.data.decode("utf-8")
-		barcodeType = barcode.type
+    # loop over the detected barcodes
+    for barcode in barcodes:
+        # the barcode data is a bytes object so we convert it to a string
+        barcodeData = barcode.data.decode("utf-8")
+        barcodeType = barcode.type
+        db = DatabaseUtils()
 
-		# if the barcode text has not been seen before print it and update the set
-		if barcodeData not in found:
-			print("[FOUND] Type: {}, Data: {}".format(barcodeType, barcodeData))
-			found.add(barcodeData)
-	
-	# wait a little before scanning again
-	time.sleep(1)
+        # if the barcode text has not been seen before 
+        # print it and update the set
+        if barcodeData not in found:
+            bookName = db.searchBook(barcodeData)
+            print("[FOUND] Type: {}, Data: {}".format(barcodeType, bookName))
+            found.add(barcodeData)
+
+    # wait a little before scanning again
+    time.sleep(1)
 
 # close the output CSV file do a bit of cleanup
 print("[INFO] cleaning up...")
---
+++ Week10/QR_code/databaseUtils.py
@@ -0,0 +1,112 @@
+import pymysql
+
+
+class DatabaseUtils():
+    HOST = "35.244.94.254"
+    USER = "root"
+    PASSWORD = "password"
+    DATABASE = "lms"
+
+    def __init__(self, connection = None):
+        if(connection == None):
+            connection = pymysql.connect(DatabaseUtils.HOST, DatabaseUtils.USER,
+                DatabaseUtils.PASSWORD, DatabaseUtils.DATABASE)
+        self.connection = connection
+
+    def close(self):
+        self.connection.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+
+    def createBookTable(self):
+        with self.connection.cursor() as cursor:
+            cursor.execute("""
+                create table if not exists Book(
+                    BookID int not null auto_increment,
+                    Name text not null,
+                    constraint FK_BookID foreign key (BookID)
+                )""")
+        self.connection.commit()
+
+    def createBorrowedBookTable(self):
+        with self.connection.cursor() as cursor:
+            cursor.execute("""
+                create table if not exists  BorrowedBook (
+                    BorrowedBook int not null auto_increment,
+                    Name text not null,
+                    constraint FK_BorrowedBook foreign key (BorrowedBook)
+                )""")
+        self.connection.commit()
+
+    def createUserTable(self):
+        with self.connection.cursor() as cursor:
+            cursor.execute("""
+                create table if not exists LmsUser (
+                    LmsUserID int not null auto_increment,
+                    Name text not null,
+                    constraint FK_UserID foreign key (UserID)
+                    constraint UN_UserName unique (UserName)
+                )""")
+        self.connection.commit()
+
+    def createEventTable(self):
+        with self.connection.cursor() as cursor:
+            cursor.execute("""
+                create table if not exists Event (
+                    EventID int not null auto_increment,
+                    Name text not null,
+                    constraint PK_EventID primary key (EventID)
+                )""")
+        self.connection.commit()
+
+    def insertBook(self, title, author ):
+        with self.connection.cursor() as cursor:
+            cursor.execute("INSERT INTO Book (Title, Author) values ((?), (?))", (title, author))
+        self.connection.commit()
+
+        return cursor.rowcount == 1
+
+    def getBook(self):
+        with self.connection.cursor() as cursor:
+            cursor.execute("select BookID, Title, Author from Book")
+            result = cursor.fetchall()
+            for row in result:
+                print ('ID:', row[0], 'TITLE:', row[1], 'AUTHOR:', row[2])
+
+    def deleteBook(self, BookID):
+        with self.connection.cursor() as cursor:
+            cursor.execute("delete from Person where BookID = %s", (BookID))
+        self.connection.commit()
+        
+    def searchBook(self, title):
+        bookID = ""
+        with self.connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM Book WHERE Title = %s", (title))
+            result = cursor.fetchall()
+            for row in result:
+                bookID = row[0]
+                print("Book Title: " + row[1] + " Author: " + row[2] )
+            return bookID
+        self.connection.commit()
+
+    def showBorrowedBooks(self, email):
+        with self.connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM BookBorrowed WHERE LmsUserID = LmsUserID FROM LmsUser WHERE Email = %s", (email))
+            result = cursor.fetchall()
+            for row in result:
+                print ('ID:', row[0], 'Title:', row[1])
+        self.connection.commit()
+
+    def returnBook(self, ID):
+        with self.connection.cursor() as cursor:
+            cursor.execute("DELETE * FROM BookBorrowed WHERE BookBorrowedID = %s", (ID))
+        self.connection.commit()
+        
+    def borrowBook(self, bookID, email):
+        with self.connection.cursor() as cursor:
+            cursor.execute("INSERT INTO BookBorrowed (BookID, Title) SELECT BookID, Title FROM Book WHERE BookID = %s AND INSERT INTO BookBorrowed Email values (%s)", (bookID, email))
+        self.connection.commit()
