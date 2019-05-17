c0956a1448b62e33e63d013cf20752709d18de35
s3599490@student.rmit.edu.au <s3599490@student.rmit.edu.au>
2019-05-17 20:21:44 +1000

    sever can be loop back

--- server.py
+++ server.py
@@ -9,25 +9,25 @@ HOST = ""
 PORT = 65001
 ADDRESS = (HOST, PORT)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind(ADDRESS)
-    s.listen()
-    
-    print("Listening on {}...".format(ADDRESS))
-    conn, addr = s.accept()
-    with conn:
-
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(ADDRESS)
+        s.listen()
         while True:
-            data = conn.recv(4096)
-            if(not data):
-                break
-            
-            user = data.decode()
-            print("User connected: " + user)
-            library_menu.display_menu(user)
-            str ="logout"
-            conn.sendall(str.encode())
-           
-        print("Disconnecting from client.")
-    print("Closing listening socket.")
-print("Done.")
+            print("Listening on {}...".format(ADDRESS))
+            conn, addr = s.accept()
+            with conn:
+                data = conn.recv(4096)
+                if(not data):
+                    break
+                
+                user = data.decode()
+                print("User connected: " + user)
+                library_menu.display_menu(user)
+                str ="logout"
+                conn.sendall(str.encode())
+        
+
+# Execute program.
+if __name__ == "__main__":
+    main()
