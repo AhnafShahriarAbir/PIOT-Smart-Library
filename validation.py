11b790d66e8b7ed9d7df421a64bceb807847cdd0
s3599490@student.rmit.edu.au <s3599490@student.rmit.edu.au>
2019-05-18 13:59:02 +1000

    Update validation.py
    documentation is updated

--- validation.py
+++ validation.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python3
+"""
+    PIOT SMART LIBRARY 
+    ~~~~~~~~~
+    This part develops and implements a robust input validation scheme.
+    :copyright: © 2019 by the PIOT group 54 team.
+    :license: BSD, see LICENSE for more details.
+"""
 import sqlite3
 from passlib.hash import sha256_crypt
 import sys
 import re
 
-databaseName='/home/pi/A2/profile.db'
-conn=sqlite3.connect(databaseName)
+databaseName='/home/pi/A2/profile.db'   #: provide the directory for local database
+conn=sqlite3.connect(databaseName)  #:make connection for the database and pi
 
 class validate():
+    """The validate class will get the user input and verify by if-elif-else statement in a while loop.
+    This class gurantees the password in a certain high level of secured form.
+    
+    """
     #The check_username() requirement user enter their user name,length should be less than 7,valid input is number,character ,"_""-"
     def check_username(self):
+        """The check_username() requirement user enter their user name with following standard,
+        - length should be less than 7
+        - number is allowed,
+        - character is allowed 
+        - special character "_" and "-" and space is allowed
+        """
         while True:
             username=input("Enter your username : ")
             if len(username) < 7:
@@ -17,21 +34,23 @@ class validate():
                     print("Valid user name")
                     break
             else :
-                False
+                False   #: loop back till matched the pattern
                 print("Invalid user name")
             
         self.username=username
         return (self.username)
-    #The check_password() will ask user enter password for twice,the password will be :
-    # length between 6 to 12 
-    # at least one character 
-    # at least one number 
-    # at least one symbol between "[@#$]" 
-    # space is not allowed
+
     def check_password(self):
+        """The check_password() will ask user enter password for twice,the password will be :
+        - length between 6 to 12 
+        - at least one character 
+        - at least one number 
+        - at least one symbol between "[@#$]" 
+        - space is not allowed
+        """
         while True:
             password=input("Enter your password : ")
-            confirmed_password=input("Enter your password again: ")   
+            confirmed_password=input("Enter your password again: ") #: ask user to type password again   
 
             if (len(password)<6 or len(password)>12):
                 print("password length is between 6 to 12,please check and enter again")
@@ -54,14 +73,17 @@ class validate():
             else:
                 print("valid password")
                 break
+        #: The sha256_Crypt.hash() function will take the value from user input and using SHA256 as the algorithm. 
+        #: SHA256 is inherently better than md5, but you're free to replace "md5" with "sha256" in our above examples to see the hash that is output still remains the same, just a bit longer.
         hashedPassword = sha256_crypt.hash(password)
 
         self.hashedPassword=hashedPassword
 
         return (self.hashedPassword)
 
-    #The check_name() will ask user to enter first name and last name, then check if it is alphabetically
     def check_name(self):
+        """The check_name() will ask user to enter first name and last name, then check if it is alphabetically or not   
+        """
         while True:
             fname=input("Enter your first name : ")
             lname=input("Enter your last name : ")
@@ -75,20 +97,30 @@ class validate():
        
         return (self.name)
 
-    #The check_email() will ask user enter email address and check the style, if email is already used,user must choose a new email to pass the next function
+    
     def check_email(self):
+        """ The check_email() will ask user enter email address and check the style, 
+        Specially email as a primary key ,it should be unique and unrepeatable in the database.
+
+        To check if it is unique, open local database and select everything in the table with the user input.
+        If find identical value,then it means the email is already signed up before and loop back.
+        If there is returning None,which proves the local database has no incerted yet,the email is valided to sign up.
+        If email is already existing in the local database,user must choose a new email to terminate the while loop.
+
+        For email,the standard form will be :"something@something.something"
+        """
         while True:
             email=input("Enter your email :")
-            curs=conn.cursor()
+            curs=conn.cursor() #:connect to the local database where list above.
             curs.execute('SELECT * FROM profile_user WHERE Email =(?)',(email,))
-            result = curs.fetchone()
+            result = curs.fetchone() #: fetch in one means only find one result
             if (result is not None):
                 False
                 print("Email is already used,please use another one.")
             elif (re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", email) ):
                 print("Valid email address")
                 #conn.close()
-                break          
+                break   #: if make the required pattern and not repeated in the database,terminate the while loop         
             else:
                 False
                 print("Invalid email,try again")
@@ -97,8 +129,15 @@ class validate():
 
         return (self.email)
 
-    #The check_login_tatus() will ask user to enter their signed email address and password,
     def check_login_status(self):
+        """The check_login_tatus() will ask user to enter their signed email address and password. Pass the emaill parameter in local database
+        to do verification. If email is existed ,go to next verification.
+
+        The sha256_crypt.verify(passWord,results[1]) validate that the two separate hashes came from the source.If the boolean rings True, then match.
+        It is because the table profile_user has row for email and hashed password , and email will be unique in the table,so if email is found and we fetch that specific row,
+        the hashed password will be taken out and verified. If the user input password is hashed with the same function,then the two source should be matched.
+        Once the boolean return true, it means log in success.
+        """
         while True:
             user_email=input("Enter your email address : ")
             passWord=input("Enter password : ")
@@ -110,7 +149,7 @@ class validate():
                 print("User is not found!")
             else:
                     if sha256_crypt.verify(passWord,results[1]):            
-                        print ("Welcome!"+results[0])                      
+                        print ("Welcome!"+results[0]) #: table profile_user colunm[0] is username.
                         break
                     else:
                         print ("Incorrect password,please check!")
