c22c6b21b7601f1fa5166cd325ad016da839ea42
s3599490@student.rmit.edu.au <s3599490@student.rmit.edu.au>
2019-05-23 14:44:37 +1000

    Create testServer.py
    for testing purpose, please comment oput the client.py library_menu(user_email)

---
+++ testServer.py
@@ -0,0 +1,26 @@
+import unittest
+import socket
+from client import Client
+
+
+class testMenu(unittest.TestCase):
+    def testServer(self):
+        HOST = "10.132.107.153" # updat the client host as per need 
+        PORT = 65001         # The port used by the server.
+        ADDRESS = (HOST, PORT)
+        useremail="111@gmail.com"
+        while True:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                print("Connecting to Server")
+                s.connect(ADDRESS)               
+                s.sendall(useremail.encode()) #: sending encoded primary key to master pi
+                data=s.recv(4096)
+                self.assertEqual(data.decode(),"logout111@gmail.com")
+                break
+        
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
